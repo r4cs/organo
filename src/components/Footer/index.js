bcd13f1214e1f5d5c114cfd04d257747a7d1a4c6
Raquel Guzansky <raquelguzansky@MacBook-Air-de-Raquel.local>
2024-01-23 14:16:45 -0300

    alteracoes css footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,19 +6,22 @@ const Footer = () => {
     return(
         <footer>
 
-            <div className="social-media">
-                <img src="/images/fb.png" alt="click to enter in facebook" />
-                <img src="/images/ig.png" alt="click to enter in instagram" />
-                <img src="/images/tw.png" alt="click to enter in twitter" />
+            <div className="footer-wrapper">
+                <div className="social-media">
+                    <img src="/images/fb.png" alt="click to enter in facebook"/>
+                    <img src="/images/ig.png" alt="click to enter in instagram"/>
+                    <img src="/images/tw.png" alt="click to enter in twitter"/>
+                </div>
+
+                <div className="logo">
+                    <img src="/images/logo.png" alt="click to enter in twitter"/>
+                </div>
+
+                <div className="development">
+                    <h6> Desenvolvido por R4CS </h6>
+                </div>
             </div>
 
-            <div className="logo">
-                <img src="/images/logo.png" alt="click to enter in twitter" />
-            </div>
-
-            <div className="development">
-                <h6> Desenvolvido por R4CS </h6>
-            </div>
 
         </footer>
     )
